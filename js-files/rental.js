43d1f35b90e35f439faac64301ab94ef6c9ba948
BeaOD <owusubea@gmail.com>
2024-09-18 13:45:50 +0000

    delivery page

--- js-files/rental.js
+++ js-files/rental.js
@@ -1,15 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const imageContainer = document.querySelector('.image-container');
-    const images = [
-        './andy-images/bus.png',
-        'image5.jpg',
-        // Add more image URLs as needed
-    ];
+// document.addEventListener("DOMContentLoaded", () => {
+//     const imageContainer = document.querySelector('.image-container');
+//     const images = [
+//         './andy-images/bus.png',
+//         'image5.jpg',
+//         // Add more image URLs as needed
+//     ];
 
-    images.forEach(src => {
-        const img = document.createElement('img');
-        img.src = src;
-        img.alt = `Image ${src}`;
-        imageContainer.appendChild(img);
-    });
-});
+//     images.forEach(src => {
+//         const img = document.createElement('img');
+//         img.src = src;
+//         img.alt = `Image ${src}`;
+//         imageContainer.appendChild(img);
+//     });
+
+
+
+    
+    AOS.init({
+      duration: 5000,
+     
+    }) ;
+  
